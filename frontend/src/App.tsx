45976cce4b79f672ca090d04a7876d78065b0983
VarunMK <mkvarun2001@gmail.com>
2022-04-13 18:01:54 +0530

    Initial Frontend Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Route,Router,Switch} from 'react-router-dom';
 import './App.css';
+import {ChakraProvider, ThemeProvider} from '@chakra-ui/react'
+import history from './history';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ChakraProvider>
+        <Router history={history}>
+          <Switch>
+            <Route path="/" exact component={Login}/>
+          </Switch>
+        </Router>
+      </ChakraProvider>
   );
 }
 
---
+++ frontend/src/Components/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Box} from "@chakra-ui/react";
+
+
+const Login = () => {
+    return (
+        <Box>
+            <h1>Login</h1>
+        </Box>
+    );
+}
+
+export default Login;
