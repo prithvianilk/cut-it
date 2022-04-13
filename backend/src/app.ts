775f025945945b545b79a4c234011a235d14bd43
Prajwal Gupta C R <prajwalguptacr@gmail.com>
2022-04-13 19:56:59 +0530

    authentication

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,13 +3,31 @@ import bodyParser from "body-parser";
 import { PORT } from "./constants";
 import otpRouter from "./otp/router";
 import orderRouter from "./order/router";
+import authRouter from "./auth/router";
+
+import mongoose from "mongoose";
+
+const db =
+	"mongodb+srv://user1:user1@cluster0.cara4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose
+	.connect(db, {
+		// useNewUrlParser: true,
+		// useCreateIndex: true,
+		// useUnifiedTopology: true,
+		// useFindAndModify: false,
+	})
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
 
 const app = Express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/otp", otpRouter);
 app.use("/order", orderRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
-  console.log(`server started on port :${PORT}.`);
+	console.log(`server started on port :${PORT}.`);
 });
