d8c35d9aa67c251b28e711abef383236520c5752
Prajwal Gupta C R <prajwalguptacr@gmail.com>
2022-04-14 08:23:37 +0530

    working on recommendations

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,18 +6,18 @@ import authRouter from "./auth/router";
 import { MONGO_DB_URI, PORT } from "./constants";
 import orderRouter from "./order/router";
 import otpRouter from "./otp/router";
-
+import recommendationsRouter from "./recommendations/router";
 
 // Connect to MongoDB
 mongoose
-  .connect(MONGO_DB_URI, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true,
-    // useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+	.connect(MONGO_DB_URI, {
+		// useNewUrlParser: true,
+		// useCreateIndex: true,
+		// useUnifiedTopology: true,
+		// useFindAndModify: false,
+	})
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
 
 const app = Express();
 app.use(cors());
@@ -26,7 +26,8 @@ app.use(bodyParser.json());
 app.use("/otp", otpRouter);
 app.use("/order", orderRouter);
 app.use("/auth", authRouter);
+app.use("/recommendations", recommendationsRouter);
 
 app.listen(PORT, () => {
-  console.log(`server started on port :${PORT}.`);
+	console.log(`server started on port :${PORT}.`);
 });
